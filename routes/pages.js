01205cfd12e452973df35e5f0788386a422649be
Sourav <chakrabortysourav801@gmail.com>
2021-12-21 15:23:03 +0530

    admin create match done

--- routes/pages.js
+++ routes/pages.js
@@ -27,9 +27,9 @@ router.get('/',async (req,res)=>{
        return res.render('index',{login:false,isAdmin:false,news:news.articles})
     }
     const email=fetchUser(token)
-
+    const user=await User.findOne({email})
     
-    res.render('index',{login:true,isAdmin:false,news:news.articles})
+    res.render('index',{login:true,isAdmin:user.isAdmin,news:news.articles})
 
 })
 let matches=[]
@@ -54,13 +54,12 @@ router.get('/fixture',async (req,res)=>{
     const userTeam=user.team
    
     userMatches=await MatchDetails.find({$or:[{'team1': { $in: userTeam }},{'team2': { $in: userTeam }}]})
-    // const userResult=await Result.find({$or:[{'team1': { $in: userTeam }},{'team2': { $in: userTeam }}]})
-
+    
     console.log("userMatches ",userMatches)
     
-    res.render('fixture',{login:true,isAdmin:false,matches,result,userM:true,userMatches})
+    res.render('fixture',{login:true,isAdmin:user.isAdmin,matches,result,userM:true,userMatches})
 })
-router.get('/Contact',(req,res)=>{
+router.get('/Contact',async (req,res)=>{
     const token=ls.get('token')
     console.log('token ',token)
     if(token===null){  
@@ -68,11 +67,11 @@ router.get('/Contact',(req,res)=>{
        return res.render('contact',{login:false,isAdmin:false})
     }
     const email=fetchUser(token)
-
+    const user=await User.find({email})
     
-    res.render('contact',{login:true,isAdmin:false})
+    res.render('contact',{login:true,isAdmin:user.isAdmin})
 })
-router.get('/Teams/:id',(req,res)=>{
+router.get('/Teams/:id',async(req,res)=>{
     const id=req.params.id
     const token=ls.get('token')
     console.log('token ',token)
@@ -87,17 +86,17 @@ router.get('/Teams/:id',(req,res)=>{
            return res.render('germany',{login:false,isAdmin:false})
     }
     const email=fetchUser(token)
-
+    const user=await User.findOne({email})
     
   
     if(id==='brazil')
-        res.render('brazil',{login:true,isAdmin:false})
+        res.render('brazil',{login:true,isAdmin:user.isAdmin})
     else if(id==='argentina')
-        res.render('argentina',{login:true,isAdmin:false})
+        res.render('argentina',{login:true,isAdmin:user.isAdmin})
     else if(id==='italy')
-        res.render('italy',{login:true,isAdmin:false})
+        res.render('italy',{login:true,isAdmin:user.isAdmin})
     else if(id==='germany')
-        res.render('germany',{login:true,isAdmin:false})
+        res.render('germany',{login:true,isAdmin:user.isAdmin})
     
     
 })
@@ -117,7 +116,8 @@ router.get('/details/:id',async (req,res)=>{
 
      }
      const email=fetchUser(token)
-     res.render('match_details',{login:true,isAdmin:false,No:match.No,date:match.date,team1:match.team1,team2:match.team2,img1:match.img1,img2:match.img2,team1_prob:match.team1_prob,team2_prob:match.team2_prob,matches:match.matches,team1_win:match.team1_win,team2_win:match.team2_win,draw:match.draw,team1_top:match.team1_top,team2_top:match.team2_top,team1_history:match.team1_history,team2_history:match.team2_history,team1_recent:match.team1_recent,team2_recent:match.team2_recent,team1_player:match.team1_player,team2_player:match.team2_player})
+     const user=await User.findOne({email})
+     res.render('match_details',{login:true,isAdmin:user.isAdmin,No:match.No,date:match.date,team1:match.team1,team2:match.team2,img1:match.img1,img2:match.img2,team1_prob:match.team1_prob,team2_prob:match.team2_prob,matches:match.matches,team1_win:match.team1_win,team2_win:match.team2_win,draw:match.draw,team1_top:match.team1_top,team2_top:match.team2_top,team1_history:match.team1_history,team2_history:match.team2_history,team1_recent:match.team1_recent,team2_recent:match.team2_recent,team1_player:match.team1_player,team2_player:match.team2_player})
 
 })
 router.get('/result/:id',async (req,res)=>{
@@ -131,7 +131,8 @@ router.get('/result/:id',async (req,res)=>{
     if(token===null)
         return res.render('result1',{login:false,isAdmin:false,No:match.No,date:match.date,team1:match.team1,team2:match.team2,team1_flag:match.team1_flag,team2_flag:match.team2_flag,team1_score:match.team1_score,team2_score:match.team2_score,team1_goal:match.team1_goal,team2_goal:match.team2_goal,team1_short:match.team1_short,team2_short:match.team2_short,team1_target:match.team1_target,team2_target:match.team2_target,team1_poss:match.team1_poss,team2_poss:match.team2_poss,team1_pass:match.team1_pass,team2_pass:match.team2_pass,team1_acc:match.team1_acc,team2_acc:match.team2_acc,team1_foul:match.team1_foul,team2_foul:match.team2_foul,team1_yellow:match.team1_yellow,team2_yellow:match.team2_yellow,team1_red:match.team1_red,team2_red:match.team2_red,team1_offside:match.team1_offside,team2_offside:match.team2_offside,team1_corner:match.team1_corner,team2_corner:match.team2_corner})
     const email=fetchUser(token)
-    res.render('result1',{login:true,isAdmin:false,No:match.No,date:match.date,team1:match.team1,team2:match.team2,team1_flag:match.team1_flag,team2_flag:match.team2_flag,team1_score:match.team1_score,team2_score:match.team2_score,team1_goal:match.team1_goal,team2_goal:match.team2_goal,team1_short:match.team1_short,team2_short:match.team2_short,team1_target:match.team1_target,team2_target:match.team2_target,team1_poss:match.team1_poss,team2_poss:match.team2_poss,team1_pass:match.team1_pass,team2_pass:match.team2_pass,team1_acc:match.team1_acc,team2_acc:match.team2_acc,team1_foul:match.team1_foul,team2_foul:match.team2_foul,team1_yellow:match.team1_yellow,team2_yellow:match.team2_yellow,team1_red:match.team1_red,team2_red:match.team2_red,team1_offside:match.team1_offside,team2_offside:match.team2_offside,team1_corner:match.team1_corner,team2_corner:match.team2_corner})
+    const user=await User.findOne({email})
+    res.render('result1',{login:true,isAdmin:user.isAdmin,No:match.No,date:match.date,team1:match.team1,team2:match.team2,team1_flag:match.team1_flag,team2_flag:match.team2_flag,team1_score:match.team1_score,team2_score:match.team2_score,team1_goal:match.team1_goal,team2_goal:match.team2_goal,team1_short:match.team1_short,team2_short:match.team2_short,team1_target:match.team1_target,team2_target:match.team2_target,team1_poss:match.team1_poss,team2_poss:match.team2_poss,team1_pass:match.team1_pass,team2_pass:match.team2_pass,team1_acc:match.team1_acc,team2_acc:match.team2_acc,team1_foul:match.team1_foul,team2_foul:match.team2_foul,team1_yellow:match.team1_yellow,team2_yellow:match.team2_yellow,team1_red:match.team1_red,team2_red:match.team2_red,team1_offside:match.team1_offside,team2_offside:match.team2_offside,team1_corner:match.team1_corner,team2_corner:match.team2_corner})
 
 
 
@@ -146,7 +147,8 @@ router.get('/player/:id',async (req,res)=>{
     if(token===null)
        return res.render('player',{login:false,isAdmin:false,id:match.id,name:match.name,img:match.img,fullName:match.fullName,born:match.born,died:match.died,age:match.age,country:match.country,height:match.height,foot:match.foot,pos:match.pos,number:match.number,match:match.match,goal:match.goal,club:match.goal,club:match.club,league:match.league,leg_match:match.leg_match,leg_goal:match.leg_goal,photo:match.photo,trophyText:match.trophyText})
     const email=fetchUser(token)
-    res.render('player',{login:true,isAdmin:false,id:match.id,name:match.name,img:match.img,fullName:match.fullName,born:match.born,died:match.died,age:match.age,country:match.country,height:match.height,foot:match.foot,pos:match.pos,number:match.number,match:match.match,goal:match.goal,club:match.goal,club:match.club,league:match.league,leg_match:match.leg_match,leg_goal:match.leg_goal,photo:match.photo,trophyText:match.trophyText})
+    const user=await User.findOne({email})
+    res.render('player',{login:true,isAdmin:user.isAdmin,id:match.id,name:match.name,img:match.img,fullName:match.fullName,born:match.born,died:match.died,age:match.age,country:match.country,height:match.height,foot:match.foot,pos:match.pos,number:match.number,match:match.match,goal:match.goal,club:match.goal,club:match.club,league:match.league,leg_match:match.leg_match,leg_goal:match.leg_goal,photo:match.photo,trophyText:match.trophyText})
 
 })
 
@@ -157,19 +159,27 @@ router.get('/profile',async (req,res)=>{
     const email=fetchUser(token)
     const user = await User.findOne({email})
     console.log(user)
-      const team=user.team.toString()
+    const team=user.team.toString()
       console.log(team)
       res.render('profile',{login:true,isAdmin:user.isAdmin,email:user.email,team:team})
 })
 router.get('/createMatch',async (req,res)=>{
-    const match=await MatchDetails.find().sort({No:'-1'}).limit(1)
-    // console.log(match)
+    const token=ls.get('token')
 
-    console.log()
+    if(token===null)
+        res.json({msg:"You need to be logged in"})
+    const email=fetchUser(token)
+    const user = await User.findOne({email})
+    if(user.isAdmin===false)
+        res.json({msg:"You need to be admin "})
+    const match=await MatchDetails.find().sort({No:'-1'}).limit(1)
+    
+
+    
     
    
 
-    res.render('match',{login:false,No:parseInt(match[0].No)+1})
+    res.render('match',{login:false,isAdmin:user.isAdmin,No:parseInt(match[0].No)+1})
 })
 
 module.exports=router
